a81dbcfe58ea28c61729048475712625c46959ba
Gordon Weakliem <gordon@vrbo.com>
2014-05-04 09:23:08 -0600

    Moved config to env

--- scratch.rb
+++ scratch.rb
@@ -1,14 +1,13 @@
 		# prompt for 
 		#  FC api key
-		api_key = "5536d57113902238"
+    api_key = ENV['FULL_CONTACT_API_KEY']
 
-		#  podio api key / secret / username / password
-		podio_api_key = 'cardreader'
-		podio_api_secret = 'dMQM1EQ7Y74z4piZQch1rVC8dH6QR0ac5NjEfJtmRde2zSuirnqOeaNUXM5iiZcV'
-		Podio.setup(:api_key => podio_api_key, :api_secret => podio_api_secret)
+    #  podio api key / secret / username / password
+    podio_api_key = ENV['PODIO_CLIENT_ID']
+    podio_api_secret = ENV['PODIO_CLIENT_SECRET']
+    Podio.setup(:api_url => 'https://api.podio.com', :api_key => podio_api_key, :api_secret => podio_api_secret)
 
-		Podio.client.authenticate_with_credentials('savvy.leverage@gmail.com', 'tool_098')
-
+    Podio.client.authenticate_with_credentials(ENV['PODIO_USERNAME'], ENV['PODIO_PASSWORD'])
 		# look up podio organizations
 		orgs = Podio::Organization.find_all
 
