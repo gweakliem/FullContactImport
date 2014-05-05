1bd94ac4ea470157af164ef7e80a5b32df56daf9
Gordon Weakliem <gordon@vrbo.com>
2014-05-05 09:21:38 -0600

    Updates to do file attachments and embeds for URLs

--- scratch.rb
+++ scratch.rb
@@ -1,6 +1,9 @@
 require 'podio'
 require 'fullcontact'
+require 'open-uri'
 require 'date'
+require 'pp'
+
 def field_values_by_external_id(external_id, options = {})
       if self.fields.present?
         field = self.fields.find { |field| field['external_id'] == external_id }
@@ -19,9 +22,33 @@ def field_values_by_external_id(external_id, options = {})
       end
     end
 
-def get_full_contact_field(record,field_id, selector_value, value_field_name)
-  field = record.find{ |i| i[field_id] == selector_value }
-  field ? field[value_field_name]:nil
+def get_full_contact_field(record,selector_key, selector_value, value_field_name, index = 0)
+  fields = record.select{ |i| i[selector_key] == selector_value } if record
+  fields ? fields[index][value_field_name]:nil
+end
+
+def create_podio_embed(record,selector_key, selector_value, value_field_name, index = 0)
+  embed_target = get_full_contact_field(record,selector_key, selector_value, value_field_name, index) 
+  embed_target ? Podio::Embed.create( embed_target ) : nil
+end
+
+def upload_file_attachment(record,selector_key, selector_value, value_field_name, label, index = 0)
+  field_value = get_full_contact_field(record,selector_key, selector_value,value_field_name, index)
+  open(field_value) { |f| Podio::FileAttachment.upload(f, label)} if field_value
+end
+
+def maybe_add_item_field(fields, field_name,record,selector_key, selector_value, value_field_name, index = 0)
+  field_value = get_full_contact_field(record,selector_key, selector_value, value_field_name, index)
+  fields[field_name]=field_value if field_value
+end
+
+def create_embed(embed_obj)
+  result = nil
+  if (embed_obj)
+    result = {"embed" => embed_obj.embed_id}
+    result["file"] = embed_obj.files[0].file_id if (embed_obj.files.length > 0)
+  end
+  return result
 end
 
 		# prompt for 
@@ -30,7 +57,6 @@ end
 
     #  podio api key / secret / username / password
     Podio.setup(:api_url => 'https://api.podio.com', :api_key => ENV['PODIO_CLIENT_ID'], :api_secret => ENV['PODIO_CLIENT_SECRET'])
-
     Podio.client.authenticate_with_credentials(ENV['PODIO_USERNAME'], ENV['PODIO_PASSWORD'])
 		# look up podio organizations
 		orgs = Podio::Organization.find_all
@@ -66,20 +92,40 @@ end
 		# for each result, import into podio
 		#cr.results.each do |result|
     result = cr.results[0]
-			Podio::Item.create(app_id,{
-				:fields => {
+    pp(result)
+      vcard_url_attach = open( result.v_card_url) { |f| Podio::FileAttachment.upload( f, "vCard") }
+      business_card_front_attach = upload_file_attachment(result.contact.photos,:type, "BusinessCard",:value, "Business Card Front")
+      pp(business_card_front_attach)
+      business_card_back_attach = upload_file_attachment(result.contact.photos,:type, "BusinessCard",:value, "Business Card Back",1) 
+      website_url_embed = create_podio_embed(result.contact.urls,:type, "Company", :value) 
+      facebook_url_embed = create_podio_embed(result.contact.accounts, :domain, "facebook.com", :url_string) 
+      twitter_url_embed = create_podio_embed(result.contact.accounts, :domain, "twitter.com", :url_string) 
+
+      new_item_fields = {
           'full-name' => result.contact.name.given_name + " " + result.contact.name.family_name,
           'title'=> get_full_contact_field(result.contact.organizations,:is_primary,true,:title),
           'company'=>get_full_contact_field(result.contact.organizations,:is_primary,true,:name),
-          'phone'=>get_full_contact_field(result.contact.phone_numbers,:type,"Work",:value),
-          'cell-phone'=>get_full_contact_field(result.contact.phone_numbers,:type,"Mobile",:value),
-          'email'=>get_full_contact_field(result.contact.emails,:type,"Work",:value),
-          'fax'=>get_full_contact_field(result.contact.phone_numbers,:type,"Work Fax",:value),
-          'street-address'=>get_full_contact_field(result.contact.addresses,:type,"Work",:street_address),
-          'city'=>get_full_contact_field(result.contact.addresses,:type, "Work",:locality ),
-          'state'=>get_full_contact_field(result.contact.addresses,:type,"Work",:region),
-          'zip'=>get_full_contact_field(result.contact.addresses,:type,"Work",:postal_code),
           "date-entered"=> {"start"=>DateTime.now.strftime("%Y-%m-%d %H:%M:%S"),"end"=>DateTime.now.strftime("%Y-%m-%d %H:%M:%S")}
       }
+
+      new_item_fields["location-met"] = "#{result.params.latitude},#{result.params.longitude}" if (result.params.latitude && result.params.longitude)
+      maybe_add_item_field(new_item_fields,'phone',result.contact.phone_numbers,:type,"Work",:value)
+      maybe_add_item_field(new_item_fields,'cell-phone',result.contact.phone_numbers,:type,"Mobile",:value)
+      maybe_add_item_field(new_item_fields,'email',result.contact.emails,:type,"Work",:value)
+      maybe_add_item_field(new_item_fields,'fax',result.contact.phone_numbers,:type,"Work Fax",:value)
+      maybe_add_item_field(new_item_fields,'street-address',result.contact.addresses,:type,"Work",:street_address)
+      maybe_add_item_field(new_item_fields,'city',result.contact.addresses,:type, "Work",:locality )
+      maybe_add_item_field(new_item_fields,'state',result.contact.addresses,:type,"Work",:region)
+      maybe_add_item_field(new_item_fields,'zip',result.contact.addresses,:type,"Work",:postal_code)
+      new_item_fields["website"] = create_embed(website_url_embed) if website_url_embed
+      #new_item_fields["twitter-url"] = create_embed(twitter_url) if twitter_url_embed
+      #new_item_fields["facebook-url"] = create_embed(facebook_url_embed) if facebook_url_embed
+      #new_item_fields["vcard-link"] =   {"url" => vcard_url_attach.link, "file" => vcard_url_attach.file_id} if vcard_url_attach
+      new_item_fields["business-card-front"] = business_card_front_attach.file_id if business_card_front_attach
+      #new_item_fields["business-card-back"] = { "url" => business_card_back_attach.link, "file" => business_card_back_attach.file_id} if business_card_back_attach
+
+      pp(new_item_fields)
+			Podio::Item.create(app_id,{
+				:fields => new_item_fields
     })
     #end
